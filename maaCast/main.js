5241612cf9c44fab83f4ed572878597a8f89917d
sujindar <sujindar.s@iiitb.org>
2020-05-31 09:45:40 +0530

    changes

--- maaCast/main.js
+++ maaCast/main.js
@@ -8,6 +8,7 @@ const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
 const uuid =  require('uuid');
 const maacastController = require('./app/controllers/maacastOrder')
+<<<<<<< HEAD
 const cart = require('./app/controllers/cart.controller')
 const maacastOrder = require('./app/models/maacast_order')
 const fs = require('fs')
@@ -69,6 +70,7 @@ app.use(session({
 app.get('/', function(req, res) {
         res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
+
 
 app.post('/initiatePayment',(req,res) => {
     console.log(req.body)
